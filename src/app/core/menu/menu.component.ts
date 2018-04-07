37ef84e069de8a0a62effb66da7c2ba94fcd0c72
Denis Lykov <cailean393@gmail.com>
2018-04-08 01:06:19 +0300

    create animated side menu

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -9,15 +9,59 @@ import { FriendsService } from '../../friends/_services/friends.service';
 })
 export class MenuComponent implements OnInit {
 
+  links: NodeListOf<HTMLElement>;
+  activeBlock: HTMLElement;
+
   constructor(
     protected storage: NgForage,
     protected friendService: FriendsService
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.getLinks();
+    this.getActiveBlock();
+    this.moveActiveBlockOnClick();
+  }
 
   async logout() {
     await this.storage.removeItem('user');
+  }
+
+  deleteActiveClass(): void {
+    for (let i = 0; i < this.links.length; i++) {
+      const element = this.links[i];
+      element.classList.remove('link-active');
+    }
+  }
+
+  getLinks(): void {
+    this.links = <NodeListOf<HTMLElement>> document.querySelectorAll('.link');
+  }
+
+  getActiveBlock(): void {
+    this.activeBlock = <HTMLElement> document.querySelector('.active-block');
+  }
+
+  moveActiveBlockOnClick(): void {
+    for (let i = 0; i < this.links.length; i++) {
+      const element = this.links[i];
+      element.onclick = () => {
+        this.activateLinkIfHasClass(element);
+        this.deleteActiveClass();
+        element.classList.add('link-active');
+        this.moveActiveBlock(element);
+      };
+    }
+  }
+
+  moveActiveBlock(el: HTMLElement): void {
+    this.activeBlock.style.top = el.offsetTop + 'px';
+  }
+
+  activateLinkIfHasClass(el: HTMLElement): void {
+    if (el.classList.contains('link-active')) {
+      this.moveActiveBlock(el);
+    }
   }
 
 }
